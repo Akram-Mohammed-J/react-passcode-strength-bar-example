78c2b1d2dad66b10811490c28cac3640c7e71030
Akram <akramm@moblize.com>
2023-01-08 11:56:03 +0530

    demo completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import { PasswordIndicator } from "react-passcode-strength-bar";
+import { useState } from "react";
 import './App.css';
 
 function App() {
+   const [passcode, setPasscode] = useState("");
+   const [email, setEmail] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <div className='container'>
+     <div>
+     <label htmlFor="email">Email</label>
+      <input
+        type="text"
+        name="password"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+     </div>
+     <div>
+     <label htmlFor="password">Password</label>
+      <input
+        type="text"
+        name="test"
+        value={passcode}
+        onChange={(e) => {
+          setPasscode(e.target.value);
+        }}
+      />
+     </div>
+     <div>
+     <PasswordIndicator
+        password={passcode}
+        show={true}
+        indicate={(value)=>console.log(value)}
+        colorConfig={{ strong: "green", moderate: "black", weak: "yellow" }}
+        containerStyle={{ 
+        marginTop: '5px',
+        borderRadius: '10px',
+        border: '1px solid #ccc',
+        padding: '3px',
+        width: '300px',
+        height: '10px',
+      }}
+      />
+     </div>
+     <button>Submit</button>
     </div>
+    </>
   );
 }
 
